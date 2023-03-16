a6a6222b0349f09479b308ef499c5bc2e6454141
realblankname1 <matthewymckinney@gmail.com>
2023-03-15 19:02:41 -0600

    main example for debugging

--- src/tetris.py
+++ src/tetris.py
@@ -286,57 +286,21 @@ if __name__ == "__main__":
     from collections import deque
 
     env = Tetris(width=10, height=20, block_size=20)
-
-    # Initialize model network, optimizer, and cost function
-    model = DQN()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-    criterion = nn.MSELoss()
-    # Reset Environment Note: should do this automatically as part of initializing environment
     state = env.reset()
-    if torch.cuda.is_available():
-        model.cuda()
-        state = state.cuda()
-
-    # Not sure
-    replay_memory = deque(maxlen=30000)
-    # resetting epochs to 0 (maybe replace with for loop)
-    epoch = 0
-    max_score = 0
-    # Intially Rendering is false until it starts to perform well
-    render = True
-    while epoch < num_epochs:
+    done = False
+    while not done:
         # Get actions and states
         next_steps = env.get_next_states()
-        # Exploration or Exploitation Scheme
-        epsilon = 1e-3 + (max(2000 - epoch, 0) * (1 - 1e-3) / 2000)
-        u = random()
-        random_action = u <= epsilon
         # Splitting up actions and related resulting states
-        next_actions, next_states = zip(*next_steps.items())
+        actions, next_states = zip(*next_steps.items())
         next_states = torch.stack(next_states)
-        # Checking for cuda
-        if torch.cuda.is_available():
-            next_states = next_states.cuda()
-        # putting model into eval mode
-        model.eval()
-        with torch.no_grad():
-            predictions = model(next_states)[:, 0]
-        # Putting model into train mode
-        model.train()
-        if random_action: # Exploration 
-            index = randint(0, len(next_steps) - 1)
-        else: # Exploitation
-            index = torch.argmax(predictions).item()
+
+        ind = randint(0, len(next_steps) - 1)
         # Indexing next state chosen by scheme
-        next_state = next_states[index, :]
-        action = next_actions[index]
+        next_state = next_states[ind]
+        action = actions[ind]
         # Obataining actual reward and whether it is done
         reward, done = env.step(action, render=True)
-
-        if torch.cuda.is_available():
-            next_state = next_state.cuda()
-        # Appending Memory
-        replay_memory.append([state, reward, next_state, done])
         # Gameover
         if done:
             final_score = env.score
@@ -352,41 +316,10 @@ if __name__ == "__main__":
                 max_score = env.score
                 render = True
             continue
-        if len(replay_memory) < 30000 / 10:
-            continue
-        epoch += 1
-        batch = sample(replay_memory, min(len(replay_memory), 512))
-        state_batch, reward_batch, next_state_batch, done_batch = zip(*batch)
-        state_batch = torch.stack(tuple(state for state in state_batch))
-        reward_batch = torch.from_numpy(np.array(reward_batch, dtype=np.float32)[:, None])
-        next_state_batch = torch.stack(tuple(state for state in next_state_batch))
 
-        if torch.cuda.is_available():
-            state_batch = state_batch.cuda()
-            reward_batch = reward_batch.cuda()
-            next_state_batch = next_state_batch.cuda()
-
-        q_values = model(state_batch)
-        model.eval()
-        with torch.no_grad():
-            next_prediction_batch = model(next_state_batch)
-        model.train()
-
-        y_batch = torch.cat(
-            tuple(reward if done else reward + 0.99 * prediction for reward, done, prediction in
-                  zip(reward_batch, done_batch, next_prediction_batch)))[:, None]
-
-        optimizer.zero_grad()
-        loss = criterion(q_values, y_batch)
-        loss.backward()
-        optimizer.step()
-
-        print("Epoch: {}/{}, Action: {}, Score: {}, Tetrominoes {}, Cleared lines: {}".format(
-            epoch,
-            num_epochs,
+        print("Action: {}, Score: {}, Tetrominoes {}, Cleared lines: {}".format(
             action,
             final_score,
             final_tetrominoes,
             final_cleared_lines))
-    video.release()
     
